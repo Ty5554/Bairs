90e200afbde56fcf2885e924d76a0d954173113d
Ty5554 <b.boowy1002@gmail.com>
2024-09-08 15:15:43 +0900

    fix_migrate

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root "home_pages#top"
   resources :users, only: %i[new create]
-  get 'login', to: 'user_sessions#new'
-  post 'login', to: 'user_sessions#create'
-  delete 'logout', to: 'user_sessions#destroy'
+  get "login", to: "user_sessions#new"
+  post "login", to: "user_sessions#create"
+  delete "logout", to: "user_sessions#destroy"
 end
