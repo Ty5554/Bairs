bdfa7f84fc10a529275667c9e87b6e9b43975bcb
Ty5554 <b.boowy1002@gmail.com>
2024-09-10 22:56:15 +0900

    create_board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,22 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.includes(:user)
   end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to boards_path, success: "投稿に成功しました"
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_created', item: Board.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def board_params
+    params.require(:board).permit(:title, :body)
+  end
 end
