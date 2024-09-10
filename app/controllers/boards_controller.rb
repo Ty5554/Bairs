616cadcc2559bd553560106981f030d040a1a504
Ty5554 <b.boowy1002@gmail.com>
2024-09-10 22:59:01 +0900

    fix_lintError

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -12,7 +12,7 @@ class BoardsController < ApplicationController
     if @board.save
       redirect_to boards_path, success: "投稿に成功しました"
     else
-      flash.now[:danger] = t('defaults.flash_message.not_created', item: Board.model_name.human)
+      flash.now[:danger] = t("defaults.flash_message.not_created", item: Board.model_name.human)
       render :new, status: :unprocessable_entity
     end
   end
